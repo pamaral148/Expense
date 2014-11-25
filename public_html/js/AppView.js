eb687c8084b5dc1edb2a9a7037904c10ab8d4e39
Paulo Amaral <pamaral@monexa.com>
2014-11-25 11:11:49 -0800

    Implemented template and form

--- public_html/js/AppView.js
+++ public_html/js/AppView.js
@@ -1,17 +1,30 @@
 (function ($) {
-    window.AppView = Backbone.View.extend({
+    AppView = Backbone.View.extend({
         el: $("body"),
         initialize: function () {
             // reference to this connects view and collection
             this.expenses = new Expenses(null, {view: this});
+            this.render();
+        },
+        render: function() {
+            var variables = { page_title: "Expenses",
+                              form_label: "New Expense",
+                              merchant_label: "Merchant",
+                              amount_label: "Amount (CAD)",
+                              date_label: "Date",
+                              submit_expense: "Add Expense",
+                              expenses_label: "Expenses" };
+            var template = _.template($("#expense-template").html(), variables);
+            $(this.el).html( template );
         },
         events: {
             "click #add-expense": "showPrompt",
         },
         showPrompt: function () {
-            var merchantName = prompt("Enter a merchant name: ");
-            var expenseDate = prompt("Enter an expense date: ");
-            var amount = prompt("Enter an amount: ");
+            var merchantName = $("#merchant-name").val();
+            var expenseDate = $("#expense-date").val();
+            var amount = $("#expense-amount").val();
+            $("#new-expense").trigger("reset");
             var expense = new Expense({merchantName: merchantName, date: expenseDate, amount: amount});
             this.expenses.add(expense);
             console.log(expense);
